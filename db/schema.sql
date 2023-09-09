fa460d905f8f03cd042d5caca68a79cc4d9a4ad0
L.J Solis <l.jsolis52@yahoo.com>
2023-09-09 18:46:50 -0500

    Created a database called employee tracker db. The databse contains tables called department, position, and employee.

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS position;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE position (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+);
