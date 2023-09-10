588ac8611f38cced0e088b4277f1b1a3938fa7b7
L.J Solis <l.jsolis52@yahoo.com>
2023-09-09 19:25:01 -0500

    Created a seeds.sql what contains information that will be inserted into the departments, job position, and the employees info.

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+VALUES
+('Stark Tower'),
+('S.H.I.E.L.D Helicarrier'),
+('Avengers Compound'),
+('Avengers Mansion'),
+('Wakanda');
+
+INSERT INTO job position (title, salary, department_id)
+VALUES
+('Avenger', 400000, 1),
+('Vigilante', 100000, 1),
+('X-MEN', 300000, 1),
+
+('Lead Scientist', 120000, 4),
+('Scientist', 65000, 4);
+
+('S.H.I.E.L.D Director', 250000, 2)
+('S.H.I.E.L.D Secretary', 80000, 2)
+('S.H.I.E.L.D Officer', 60000, 2)
+('S.H.I.E.L.D Accountant', 90000, 2)
+
+('Custodian', 50000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Peter', 'Parker', 4, NULL),
+('Tony', 'Stark', 2, 1),
+('Gwen', 'Stacy', 7, 5),
+('Bruce', 'Banner', 6, 3),
+('Natasha', 'Romanoff', 4, NULL)
+('Steve', 'Rogers', 5, 2),
+('Thor', 'Son of Odin', 9, 4);
+
